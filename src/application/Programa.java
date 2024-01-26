41b93ca8812b17e6310f9e7a51dba59905e2d47b
Letícia <leticia_isabelah@hotmail.com>
2024-01-26 11:36:03 -0300

    Cração das classes VendedorDaoJDB e DaoFactory

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.VendedorDAO;
 import model.entities.Departamento;
 import model.entities.Vendedor;
 
@@ -12,6 +14,8 @@ public class Programa {
 		Departamento depart = new Departamento(1, "Livros");
 		
 		Vendedor vendedor = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.0, depart);
+		
+		VendedorDAO sellerDao = DaoFactory.createSellerDAO();
 		
 		System.out.println(depart);
 		System.out.println(vendedor);
