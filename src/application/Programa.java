d4b71b4d984b37951c94f3e88750a429ad840065
Letícia <leticia_isabelah@hotmail.com>
2024-01-26 12:11:05 -0300

    Implementação do findById

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,23 +1,17 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.VendedorDAO;
-import model.entities.Departamento;
 import model.entities.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Departamento depart = new Departamento(1, "Livros");
-		
-		Vendedor vendedor = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.0, depart);
-		
 		VendedorDAO sellerDao = DaoFactory.createSellerDAO();
 		
-		System.out.println(depart);
-		System.out.println(vendedor);
+		Vendedor seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 	}
 }
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.VendedorDaoJDBC;
 
 public class DaoFactory {
 	
 	public static VendedorDAO createSellerDAO() {
-		return new VendedorDaoJDBC();
+		return new VendedorDaoJDBC(DB.getConnection());
 	}
 
 }
