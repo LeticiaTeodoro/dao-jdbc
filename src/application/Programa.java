1d69bf11a6c8598631f547f769df6c6c4cb32887
Letícia <leticia_isabelah@hotmail.com>
2024-01-26 11:20:50 -0300

    Criação da classe venddor

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departamento;
+import model.entities.Vendedor;
 
 public class Programa {
 
@@ -8,6 +11,9 @@ public class Programa {
 		
 		Departamento depart = new Departamento(1, "Livros");
 		
+		Vendedor vendedor = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.0, depart);
+		
 		System.out.println(depart);
+		System.out.println(vendedor);
 	}
 }
